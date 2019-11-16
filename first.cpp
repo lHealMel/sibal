fce33e4a0b6b00b1e3ee5493bdaa111d86adc825
lHealMel <50746821+lHealMel@users.noreply.github.com>
2019-11-16 11:31:10 +0900

    fdas
    fads

---
+++ first.cpp
@@ -0,0 +1,44 @@
+// ConsoleApplication2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+#include <stdio.h>
+#include <conio.h>
+
+#define UP 72
+#define DOWN 80
+#define LEFT 75 
+#define RIGHT 77
+#define SPACEBAR 32
+
+
+
+int main()
+{
+	while (kbhit()) {
+		char key = getch();
+		switch (key)
+		{
+		case UP:
+
+
+		case DOWN:
+
+
+		case LEFT:
+
+
+		case RIGHT:
+
+
+		}
+		if (key == SPACEBAR) {
+
+
+		}
+
+
+
+	 }
+}
+
